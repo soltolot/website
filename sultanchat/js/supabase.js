94688a6e03625378854342fe30ec4b3ab6ce1d5c
soltolot <sultanalawad2013@gmail.com>
2026-05-15 18:33:14 +0300

    Update supabase.js

--- sultanchat/js/supabase.js
+++ sultanchat/js/supabase.js
@@ -1,1 +1,3 @@
-
+const SUPABASE_URL = "https://vldkmlbfjzdrfzpeyver.supabase.co";
+const SUPABASE_KEY = "sb_publishable_3e7UaMtOS1nZawcvFHYGhA_-RSInVmg";
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
