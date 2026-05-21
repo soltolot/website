5f2d8e5945f233d6c0a7b96d02d14d6c5db3a2d5
soltolot <sultanalawad2013@gmail.com>
2026-05-21 13:12:13 +0400

    Update chat.js

--- sultanchat/js/chat.js
+++ sultanchat/js/chat.js
@@ -1,11 +1,4 @@
 // --------------------------------------------------
-// Supabase Init (ONLY ONCE)
-// --------------------------------------------------
-const SUPABASE_URL = "https://vldkmlbfjzdrfzpeyver.supabase.co";
-const SUPABASE_KEY = "sb_publishable_3e7UaMtOS1nZawcvFHYGhA_-RSInVmg";
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-// --------------------------------------------------
 // Better log() function (supports multiple arguments)
 // --------------------------------------------------
 function log(...args) {
