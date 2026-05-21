2db342c4a18faa4e5869057cb53c81c6ff3cb310
soltolot <sultanalawad2013@gmail.com>
2026-05-21 12:52:23 +0400

    Update chat.js

--- sultanchat/js/chat.js
+++ sultanchat/js/chat.js
@@ -1,8 +1,3 @@
-//<script>
-const SUPABASE_URL = "https://vldkmlbfjzdrfzpeyver.supabase.co";
-const SUPABASE_KEY = "sb_publishable_3e7UaMtOS1nZawcvFHYGhA_-RSInVmg";
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
 window.displayName = null;
 
 // ⭐ Load username from session
